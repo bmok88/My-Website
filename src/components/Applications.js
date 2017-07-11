550f308ebf57148a0ea680d2473800506c75b4a9
Brendan Mok <bmok88@gmail.com>
2017-07-10 17:29:06 -0700

    Change to s3 buckets

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -16,21 +16,21 @@ class Applications extends React.Component {
       <div id="applications">
         <div id="applications-header">Full Stack Applications</div>
           <div className="app" ref="gauntlet">
-            <div className="app image"><img src="Gauntlet.png"/></div>
+            <div className="app image"><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/Gauntlet.png"/></div>
             <button id="view" className="btn btn-default" onClick={() => this.handleButtonClick("gaunt")}>View</button>
             <a href="https://github.com/bmok88/TheGauntlet" target="_blank"><button id="git" className="btn btn-default">Github</button></a>
             <p className="apptitle">The Gauntlet</p>
             <p>React/Redux, Node.js/Express, AWS S3, MySQL</p>
           </div>
           <div className="app" ref="wefitly">
-            <div className="app image"><img src="http://i.imgur.com/0otzFlt.png"/></div>
+            <div className="app image"><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/Screen+Shot+2017-03-08+at+3.52.42+PM.png"/></div>
             <button id="view" className="btn btn-default" onClick={() => this.handleButtonClick("wefit")}>View</button>
             <a href="https://github.com/wefitly/wefitly" target="_blank"><button id="git" className="btn btn-default">Github</button></a>
             <p className="apptitle">WeFitly</p>
             <p>React, Node.js/Express, MongoDB/Mongoose</p>
           </div>
           <div className="app" ref="rainsong">
-            <div className="app image"><img src="http://i.imgur.com/ZHALw76.jpg"/></div>
+            <div className="app image"><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/Screen+Shot+2017-03-08+at+5.34.24+PM.png"/></div>
             <button id="view" className="btn btn-default" onClick={() => this.handleButtonClick("rain")}>View</button>
             <a href="https://github.com/bmok88/RainSong/tree/playList" target="_blank"><button id="git" className="btn btn-default">Github</button></a>
             <p className="apptitle">RainSong</p>
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,22 +4,22 @@ const Interests = () => (
   <div id="interests">
     <div id="interests-header">Software Interests</div>
     <div className="logo web">
-      <div><img src="https://d2ln17qvd2qkrp.cloudfront.net/wp-content/uploads/service_blurb-web_development.png" /></div>
+      <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/web_dev_icon.png" /></div>
       <p className="interest">Web Development</p>
       <p>Creating responsive and interactive web applications with modern technologies that exhibit beautiful designs and exceptional features to optimize UX.</p>
     </div>
     <div className="logo js">
-       <div><img src="https://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/082014/js1_0.png?itok=BqOjTfHU" /></div>
+       <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/js_logo.png" /></div>
       <p className="interest">JavaScript</p>
       <p>Understanding the finest details of this powerful language and utilizing it to create software, design algorithms, and of course mess around with.</p>
     </div>
     <div className="logo api">
-       <div><img src="http://yvesfrateur.be/wp-content/uploads/2015/09/api-icon.png" /></div>
+       <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/api_logo.png" /></div>
       <p className="interest">API Design</p>
       <p>Developing RESTful API's with best practices such as RESTful URL's and actions, query parameters, HTTP status codes, JSON, caching, and appropriate headers.</p>
     </div>
     <div className="logo system">
-      <div><img src="Webalys-Kameleon.pics-Graph-Magnifier (1).ico" /></div>
+      <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/Webalys-Kameleon.pics-Graph-Magnifier+(1).ico" /></div>
       <p className="interest">System Design</p>
       <p>Architecting smart, scalable designs to common problems and bottlenecks that provide creative solutions from the client to server to database.</p>
     </div>
@@ -27,11 +27,11 @@ const Interests = () => (
       Future Endeavors...
     </div>
     <div className="logo mobile">
-       <div><img src="https://static1.squarespace.com/static/54691977e4b0773873087bc2/t/54ab600ce4b010b9ae3a9bef/1420517389330/Doozy+Labs+Mobile+Development+Icon" /></div>
+       <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/mobile_logo.png" /></div>
       <p className="interest">Mobile Development</p>
     </div>
     <div className="logo game">
-       <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Vg_development_icon.svg/200px-Vg_development_icon.svg.png"/></div>
+       <div><img src="https://s3-us-west-1.amazonaws.com/brendanmok.herokuapp.com/game_logo.png"/></div>
       <p className="interest">Game Development</p>
     </div>
   </div>
